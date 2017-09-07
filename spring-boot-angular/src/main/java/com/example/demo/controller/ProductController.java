39f6632f7f5b7dcf09d866c48d116f1ee7b8a9db
jamie <752492509@qq.com>
2017-09-07 22:29:11 +0800

    update angular crud

--- spring-boot-angular/src/main/java/com/example/demo/controller/ProductController.java
+++ spring-boot-angular/src/main/java/com/example/demo/controller/ProductController.java
@@ -7,7 +7,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.entity.Product;
+import com.example.demo.repository.Product;
 
 
 @RestController
--- spring-boot-angular/src/main/java/com/example/demo/repository/Product.java
+++ spring-boot-angular/src/main/java/com/example/demo/repository/Product.java
@@ -1,4 +1,4 @@
-package com.example.demo.entity;
+package com.example.demo.repository;
 
 public class Product{
 	private String id;
