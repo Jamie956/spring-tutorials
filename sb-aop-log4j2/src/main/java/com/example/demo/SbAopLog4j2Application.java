f8308eec0b96d218f8d663c395eb38f475a5dce8
jamie <jamie@justek.us>
2017-10-21 01:58:31 +0800

    init aop log4j2

---
+++ sb-aop-log4j2/src/main/java/com/example/demo/SbAopLog4j2Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbAopLog4j2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbAopLog4j2Application.class, args);
+	}
+}
