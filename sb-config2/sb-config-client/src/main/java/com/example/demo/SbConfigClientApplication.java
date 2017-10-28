90e46efa1cebd3176161fe766980e6030f1c8ed0
jamie <jamie@justek.us>
2017-10-28 23:28:04 +0800

    init config2

---
+++ sb-config2/sb-config-client/src/main/java/com/example/demo/SbConfigClientApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbConfigClientApplication.class, args);
+	}
+}
