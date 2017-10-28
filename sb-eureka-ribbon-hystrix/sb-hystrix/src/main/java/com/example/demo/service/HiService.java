ff7e44281fe66f8a2075b25652d750c5acbb24f3
jamie <jamie@justek.us>
2017-10-28 19:21:41 +0800

    init hystrix

--- sb-eureka-ribbon-hystrix/sb-hystrix/src/main/java/com/example/demo/service/HiService.java
+++ sb-eureka-ribbon-hystrix/sb-hystrix/src/main/java/com/example/demo/service/HiService.java
@@ -1,17 +1,21 @@
 package com.example.demo.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class HiService {
-
     @Autowired
     RestTemplate restTemplate;
 
-    public String hiService() {
+    @HystrixCommand(fallbackMethod = "hiError")
+    public String sayhi() {
         return restTemplate.getForObject("http://service-hi",String.class);
     }
 
+    public String hiError() {
+        return "sorry, error!";
+    }
 }
