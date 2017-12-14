5321ecd370f6f3ad253901589887d4c3027219d4
jamie <jamie@justek.us>
2017-12-14 16:44:24 +0800

    update config

--- sb-config/src/test/java/com/example/demo/SbConfigApplicationTests.java
+++ sb-config/src/test/java/com/example/demo/SbConfigApplicationTests.java
@@ -2,15 +2,28 @@ package com.example.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SbConfigApplicationTests {
-
+	@Value("${com.jamie.name}")
+    private String name;
+	
+	@Autowired
+	private Random random;
+	
 	@Test
-	public void contextLoads() {
+	public void test1() {
+		System.out.println("name => "+name);
 	}
-
+	
+	@Test
+	public void test2() {
+		random.hi();
+	}
+	
 }
