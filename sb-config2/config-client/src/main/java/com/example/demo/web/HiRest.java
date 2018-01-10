d875d9c4972f1caa2f6d177727188b045fa53e35
jamie <jamie@justek.us>
2018-01-10 16:41:00 +0800

    config

--- sb-config2/config-client/src/main/java/com/example/demo/web/HiRest.java
+++ sb-config2/config-client/src/main/java/com/example/demo/web/HiRest.java
@@ -5,15 +5,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HiController {
+public class HiRest {
 	@Value("${foo}")
 	private String foo;
-	
-	@Value("${a}")
-	private String a;
-	
+
+	@Value("${bar}")
+	private String bar;
+
 	@RequestMapping(value = "/")
-	public String get(){
-		return foo +"-"+ a;
+	public String get() {
+		return foo + "; " + bar;
 	}
 }
