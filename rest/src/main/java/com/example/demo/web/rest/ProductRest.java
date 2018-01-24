a0e616a350d7ca18458184eca9430ea82ff1fb69
jamie <jamie@justek.us>
2018-01-24 16:01:37 +0800

    rename rest

---
+++ rest/src/main/java/com/example/demo/web/rest/ProductRest.java
@@ -0,0 +1,45 @@
+package com.example.demo.web.rest;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.entity.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
+public class ProductRest {
+	
+	@RequestMapping(method = RequestMethod.GET, value = "")
+	public List<Product> findAll() {
+		List<Product> ps = new ArrayList<Product>();
+		ps.add(new Product("1", "cpu", "99"));
+		ps.add(new Product("2", "main", "99"));
+		return ps;
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "/{id}")
+	public Product findById(@PathVariable("id") String id) {
+		System.out.println("id => " + id);
+		return new Product("3", "un", "99");
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "")
+	public String save(@RequestBody Product product) {
+		System.out.println("product => " + product);
+		return "ok";
+	}
+
+	@RequestMapping(method = RequestMethod.PUT, value = "/{id}")
+	public String update(@RequestBody Product product) {
+		System.out.println("product => " + product);
+		return "ok";
+	}
+
+	@RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
+	public String remove(@PathVariable("id") String id) {
+		System.out.println("id => " + id);
+		return "ok";
+	}
+}
--- sb-start/README.md
+++
@@ -1,9 +0,0 @@
-method:get => localhost:8080/product
-method:post => localhost:8080/product/01
-method:post, application/json => localhost:8080/product
-	{
-	    "id": "05",
-	    "name": "product05",
-	    "price": "2.00"
-	}
-method:put => localhost:8080/product/01
