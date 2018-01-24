c1a044611689e9b17230caf9c8149f2d7fa0b421
jamie <jamie@justek.us>
2018-01-24 16:20:08 +0800

    rename upload

--- upload/src/main/java/com/example/demo/web/rest/UploadRest.java
+++ upload/src/main/java/com/example/demo/web/rest/UploadRest.java
@@ -1,4 +1,4 @@
-package com.example.demo.controller;
+package com.example.demo.web.rest;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
@@ -7,32 +7,23 @@ import java.nio.file.Paths;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.demo.util.FileUtils;
 
+@RestController
+public class UploadRest {
 
-@Controller
-public class MainController {
-	private static final Logger logger = LogManager.getLogger(MainController.class);
-	
-	@RequestMapping("/")
-	public String index() {
-		return "index";
-	}
-
-	@RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
+	@RequestMapping(value = "/upload", method = RequestMethod.POST)
 	@ResponseBody
-	public ResponseEntity<?> uploadFile(@RequestParam("uploadfile") MultipartFile uploadfile, HttpServletRequest request) {
+	public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile uploadfile, HttpServletRequest request) {
 		try {
 			String filename = uploadfile.getOriginalFilename();
 			String directory = request.getContextPath() + "upload";
@@ -42,15 +33,12 @@ public class MainController {
 			BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(filepath));
 			stream.write(uploadfile.getBytes());
 			stream.close();
-			logger.info("creating file => " + filepath);
-			
-//			FileUtils.removeFile(filepath);
-			
+			System.out.println("creating file => " + filepath);
+			// FileUtils.removeFile(filepath);
 		} catch (Exception e) {
-			logger.error(e.getMessage());
+			System.out.println(e.getMessage());
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 		}
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
-
 }
