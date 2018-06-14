15057dfc8197660de42eb1a0c87a8ecf30401a72
jamie956 <752492509@qq.com>
2018-06-14 21:25:28 +0800

    update mybatis 2

--- mybatis-xml2/src/main/java/com/example/demo/mapper/CategoryMapper.java
+++ mybatis-xml2/src/main/java/com/example/demo/mapper/CategoryMapper.java
@@ -1,10 +1,25 @@
 package com.example.demo.mapper;
 
 import java.util.List;
+import java.util.Map;
 
 import com.example.demo.entity.Category;
 
 
 public interface CategoryMapper {
 	List<Category> listCategory0();
+	
+	void addCategory(Category category);
+	
+	void deleteCategory(Category category);
+	
+	Category getCategory(int id);
+	
+	void updateCategory(Category category);
+	
+	List<Category> listCategoryByName(String name);
+	
+	List<Category> listCategoryByIdAndName(Map<String,Object> map);
+	
+	List<Category> listCategory2();
 }
--- mybatis-xml2/src/test/java/com/example/demo/dao/CategoryDaoTest.java
+++ mybatis-xml2/src/test/java/com/example/demo/dao/CategoryDaoTest.java
@@ -1,6 +1,8 @@
 package com.example.demo.dao;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.example.demo.entity.Category;
+import com.example.demo.entity.Product;
 import com.example.demo.mapper.CategoryMapper;
 
 @RunWith(SpringRunner.class)
@@ -23,6 +26,64 @@ public class CategoryDaoTest {
 		List<Category> cs =  categoryMapper.listCategory0();
 		for(Category c:cs) {
 			System.out.println(c);
+		}
+	}
+	
+	@Test
+	public void simpleInsert() {
+		Category c = new Category();
+		c.setName("new Category");
+		categoryMapper.addCategory(c);
+	}
+	
+	@Test
+	public void simpleDelete() {
+		Category c = new Category();
+		c.setId(2);
+		categoryMapper.deleteCategory(c);
+	}
+	
+	@Test
+	public void simpleFindById() {
+		Category rs = categoryMapper.getCategory(1);
+		System.out.println(rs);
+	}
+	
+	@Test
+	public void simpleUpdate() {
+		Category c = categoryMapper.getCategory(4);
+		c.setName("update category");
+		categoryMapper.updateCategory(c);
+	}
+	
+	@Test
+	public void simpleListByName() {
+		List<Category> cs = categoryMapper.listCategoryByName("cat");
+		for(Category c : cs){
+			System.out.println(c);
+		}
+	}
+	
+	@Test
+	public void simpleListIdByName() {
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("id", 1);
+		params.put("name", "cat");
+		List<Category> cs = categoryMapper.listCategoryByIdAndName(params);
+		for(Category c : cs){
+			System.out.println(c);
+		}
+	}
+	
+	@Test
+	public void listOneToMany() {
+		List<Category> cs = categoryMapper.listCategory2();
+		for(Category c : cs){
+			System.out.println(c);
+			List<Product> ps = c.getProducts();
+			for (Product p : ps) {
+				System.out.println("\t" + p);
+			}
 		}
 	}
 	
