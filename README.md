61daba8c40def2e8dcfc10728b61d4f3569d2a90
Jamie956 <752492509@qq.com>
2020-07-31 00:27:33 +0800

    request filter wrapper

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 ## hi spring boot 
 
 [admin](/admin/README.md). 
+
+
+
+## request_wrapper
+
+请求拦截，修改参数
---
+++ request_wrapper/src/main/java/com/example/TokenFilter.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Component
+public class TokenFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        chain.doFilter(new TokenRequestWrapper((HttpServletRequest) req), res);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
