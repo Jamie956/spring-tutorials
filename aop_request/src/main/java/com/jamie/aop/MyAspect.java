cec930a9ea71ef3ade0b7382ea3deb8f0e11326e
Jamie956 <752492509@qq.com>
2020-08-14 20:26:50 +0800

    up

--- aop_request/src/main/java/com/jamie/aop/MyAspect.java
+++ aop_request/src/main/java/com/jamie/aop/MyAspect.java
@@ -7,9 +7,13 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
-import java.util.Arrays;
-import java.util.List;
+import javax.servlet.http.HttpServletRequest;
 
 @Aspect
 @Component
@@ -22,8 +26,19 @@ public class MyAspect {
     @Around("paramPointCut()")
     public Object balabala(ProceedingJoinPoint joinPoint) throws Throwable {
 
-        MethodSignature msg = (MethodSignature) joinPoint.getSignature();
-        List<String> paramNameList = Arrays.asList(msg.getParameterNames());
+//        MethodSignature msg = (MethodSignature) joinPoint.getSignature();
+//        List<String> paramNameList = Arrays.asList(msg.getParameterNames());
+
+        String classRequestMappingValue = joinPoint.getTarget().getClass().getAnnotation(RequestMapping.class).value()[0];
+        String methodPostMappingValue = ((MethodSignature)joinPoint.getSignature()).getMethod().getAnnotation(PostMapping.class).value()[0];
+
+
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        String[] pathArrary = StringUtils.split(request.getRequestURI(), "/");
+        String path = "/" + pathArrary[pathArrary.length-1] + "/" + pathArrary[pathArrary.length-2];
+
+
+
 
         Object[] args = joinPoint.getArgs();
 
