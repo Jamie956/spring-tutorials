ce772216e7344b725ca50c7ecd1bb5909e8a0311
Jamie956 <752492509@qq.com>
2020-08-19 22:22:11 +0800

    up

---
+++ spring-cloud-alibaba/spring-cloud-alibaba-nacos-consumer/src/main/java/com/jamie/spring/cloud/alibaba/nacos/consumer/config/NacosConsumerConfiguration.java
@@ -0,0 +1,15 @@
+package com.jamie.spring.cloud.alibaba.nacos.consumer.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class NacosConsumerConfiguration {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
