218778b1036d929daf00afe5232dc6e71efb605b
Jamie956 <752492509@qq.com>
2020-08-21 17:09:25 +0800

    update interceptor

--- all-in-one/interceptor/src/main/java/com/jamie/config/MvcConfig.java
+++ all-in-one/interceptor/src/main/java/com/jamie/config/MvcConfig.java
@@ -8,14 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import javax.annotation.Resource;
 
 @Configuration
-public class MyWebAppConfigurer implements WebMvcConfigurer {
+public class MvcConfig implements WebMvcConfigurer {
 
-    @Resource
-    private MyInterceptor myInterceptor;
+//    @Resource
+//    private MyInterceptor myInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         //对来自/** 全路径请求进行拦截
-        registry.addInterceptor(myInterceptor).addPathPatterns("/**");
+//        registry.addInterceptor(myInterceptor).addPathPatterns("/**");
+        registry.addInterceptor(new MyInterceptor());
     }
 }
--- all-in-one/interceptor/src/main/java/com/jamie/interceptor/MyInterceptor.java
+++ all-in-one/interceptor/src/main/java/com/jamie/interceptor/MyInterceptor.java
@@ -7,23 +7,35 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@Component
+//@Component
 public class MyInterceptor implements HandlerInterceptor {
 
+    /**
+     * 进入Controller层之前拦截请求，默认是拦截所有请求
+     * @return 是否拦截当前请求，true表示拦截当前请求，false表示不拦截当前请求
+     */
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         //获取注解
         //((HandlerMethod) handler).getMethod().getAnnotation(MyAnnotation.class);
         System.out.println("preHandle");
+        //请求路径
+        String requestURI = request.getRequestURI();
         return true;
     }
 
+    /**
+     * 处理完请求后但还未渲染视图之前进行的操作
+     */
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
         System.out.println("postHandle");
 
     }
 
+    /**
+     * 视图渲染后但还未返回到客户端时的操作
+     */
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         System.out.println("afterCompletion");
