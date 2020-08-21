ca736f5a8f38d8c9d93ffb7e55775b849d49cbc5
Jamie956 <752492509@qq.com>
2020-08-21 16:56:07 +0800

    update interceptor

--- all-in-one/interceptor/src/main/java/com/jamie/config/MyWebAppConfigurer.java
+++ all-in-one/interceptor/src/main/java/com/jamie/config/MyWebAppConfigurer.java
@@ -1,21 +1,21 @@
 package com.jamie.config;
 
 import com.jamie.interceptor.MyInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
 
 @Configuration
-public class MyWebAppConfigurer extends WebMvcConfigurerAdapter {
+public class MyWebAppConfigurer implements WebMvcConfigurer {
 
-    @Autowired
+    @Resource
     private MyInterceptor myInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         //对来自/** 全路径请求进行拦截
         registry.addInterceptor(myInterceptor).addPathPatterns("/**");
-        super.addInterceptors(registry);
     }
 }
