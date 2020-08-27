81f59701c532c425c29e109e22b651d6b9715d0c
Jamie956 <752492509@qq.com>
2020-08-27 10:02:14 +0800

    update filter handle request body

--- filter-interceptor-aop/filter/src/main/java/com/jamie/controller/FilterController.java
+++ filter-interceptor-aop/filter/src/main/java/com/jamie/controller/FilterController.java
@@ -1,22 +1,12 @@
 package com.jamie.controller;
 
 import com.jamie.entity.Greeting;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-
-import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class FilterController {
-    @GetMapping("hi")
-    public void sayHi() {
-        System.out.println("halo");
-    }
-
     /*
     post
     localhost:8085/greet
@@ -26,9 +16,7 @@ public class FilterController {
     }
      */
     @PostMapping("greet")
-    public void sayHi2(@RequestBody Greeting greet) {
-        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-
+    public void sayHi(@RequestBody Greeting greet) {
         System.out.println(greet);
     }
 }
--- filter-interceptor-aop/filter/src/main/java/com/jamie/filter/MyFilter.java
+++ filter-interceptor-aop/filter/src/main/java/com/jamie/filter/MyFilter.java
@@ -1,13 +1,7 @@
 package com.jamie.filter;
 
 import com.alibaba.fastjson.JSON;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.http.HttpInputMessage;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.FormHttpMessageConverter;
-import org.springframework.http.converter.support.AllEncompassingFormHttpMessageConverter;
-import org.springframework.http.server.ServletServerHttpRequest;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -17,55 +11,56 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
 
 @Component
 public class MyFilter extends OncePerRequestFilter {
-    private FormHttpMessageConverter formConverter = new AllEncompassingFormHttpMessageConverter();
-    public static final Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
-    private Charset charset = DEFAULT_CHARSET;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        //获取请求 body inputStream -> string
-        HttpInputMessage inputMessage = new ServletServerHttpRequest(request);
-        MediaType contentType = inputMessage.getHeaders().getContentType();
-        Charset charset = (contentType != null && contentType.getCharset() != null ? contentType.getCharset() : this.charset);
-        String body = StreamUtils.copyToString(inputMessage.getBody(), charset);
+        //构造RequestWrapper，并将输入流转成byte[]，存到变量requestBody
+        HttpBodyContentRequestWrapper requestWrapper = new HttpBodyContentRequestWrapper(request);
+        //获取body json，将body byte[]转换成json
+        JSONObject json = requestWrapper.getRequestBody();
 
-        //string -> map
-        Map<String, Object> paramMap = new ObjectMapper().readValue(body, new TypeReference<HashMap<String, Object>>() {
-        });
-
-        //修改body
-        for (Map.Entry<String, Object> entry : paramMap.entrySet()) {
-            entry.setValue("22222");
-        }
-
-        //map -> bytes -> byteArrayInputStream
-        byte[] bytes = JSON.toJSONString(paramMap).getBytes(charset);
-        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
-
-        //创建新的requestWrapper
-        HttpServletRequestWrapper requestWrapper = new HttpPutBodyContentRequestWrapper(request, byteArrayInputStream);
+        //更改body，将json 转成 body byte[]
+        requestWrapper.setRequestBody(JSON.parseObject("{'id':2,'greet':'hihi'}"));
 
         filterChain.doFilter(requestWrapper, response);
     }
 
-    private static class HttpPutBodyContentRequestWrapper extends HttpServletRequestWrapper {
-        private final ByteArrayInputStream byteArrayInputStream;
+    private static class HttpBodyContentRequestWrapper extends HttpServletRequestWrapper {
+        private byte[] requestBody = null;
 
-        public HttpPutBodyContentRequestWrapper(HttpServletRequest request, ByteArrayInputStream byteArrayInputStream) {
+        public JSONObject getRequestBody() throws UnsupportedEncodingException {
+            return JSON.parseObject((new String(requestBody, "UTF-8")));
+        }
+
+        public void setRequestBody(JSONObject jsonObject) throws UnsupportedEncodingException {
+            this.requestBody = jsonObject.toJSONString().getBytes("UTF-8");
+        }
+
+        public HttpBodyContentRequestWrapper(HttpServletRequest request) {
             super(request);
-            this.byteArrayInputStream = byteArrayInputStream;
+            try {
+                requestBody = StreamUtils.copyToByteArray(request.getInputStream());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
         @Override
-        public ServletInputStream getInputStream() throws IOException {
+        public ServletInputStream getInputStream() {
+            if (requestBody == null) {
+                requestBody = new byte[0];
+            }
+            final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(requestBody);
+
             return new ServletInputStream() {
+                @Override
+                public int read() throws IOException {
+                    return byteArrayInputStream.read();
+                }
+
                 @Override
                 public boolean isFinished() {
                     return false;
@@ -77,20 +72,15 @@ public class MyFilter extends OncePerRequestFilter {
                 }
 
                 @Override
-                public void setReadListener(ReadListener readListener) {
-                }
+                public void setReadListener(ReadListener listener) {
 
-                @Override
-                public int read() throws IOException {
-                    return byteArrayInputStream.read();
                 }
             };
         }
-
         @Override
         public BufferedReader getReader() throws IOException {
             return new BufferedReader(new InputStreamReader(getInputStream()));
         }
-
     }
+
 }
