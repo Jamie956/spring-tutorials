501ad8c8367a8d7ec8695e75545230e30a981e37
Jamie956 <752492509@qq.com>
2020-08-30 16:04:03 +0800

    eureka cluster

--- springcloud2020/cloud-eureka-server7002/src/main/java/com/jamie/springcloud/EurekaServerMain7002.java
+++ springcloud2020/cloud-eureka-server7002/src/main/java/com/jamie/springcloud/EurekaServerMain7002.java
@@ -2,20 +2,13 @@ package com.jamie.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-/**
- * 集群访问
- * localhost:7001
- * localhost:7002
- * eureka7001.com:7001
- * eureka7002.com:7002
- *
- */
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServerMain7001 {
+public class EurekaServerMain7002 {
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerMain7001.class, args);
+        SpringApplication.run(EurekaServerMain7002.class, args);
     }
 }
