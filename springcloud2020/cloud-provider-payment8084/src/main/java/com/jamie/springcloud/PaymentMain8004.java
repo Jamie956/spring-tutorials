db7dc7b32b5874baf54dce3f38450504aee9070f
Jamie956 <752492509@qq.com>
2020-08-31 23:58:43 +0800

    add zk

---
+++ springcloud2020/cloud-provider-payment8084/src/main/java/com/jamie/springcloud/PaymentMain8004.java
@@ -0,0 +1,16 @@
+package com.jamie.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * localhost:8004/payment/zk
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain8004 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8004.class, args);
+    }
+}
