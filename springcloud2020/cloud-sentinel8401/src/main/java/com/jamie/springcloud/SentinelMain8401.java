178b3c70f832adee1c2dc9def4cb41dec24cc725
Jamie956 <752492509@qq.com>
2020-09-12 16:18:38 +0800

    add sentinel

---
+++ springcloud2020/cloud-sentinel8401/src/main/java/com/jamie/springcloud/SentinelMain8401.java
@@ -0,0 +1,18 @@
+package com.jamie.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 触发sentinel控制台显示，测试设置流量控制的效果
+ * http://localhost:8401/testA
+ * http://localhost:8401/testB
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SentinelMain8401 {
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelMain8401.class, args);
+    }
+}
