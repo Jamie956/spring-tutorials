59572680eac5b878ccdadf8ad4d7abf2b97a24fd
Jamie956 <752492509@qq.com>
2020-09-13 21:35:45 +0800

    sentinel fallback error page

---
+++ springcloud2020/cloud-sentinel8401/src/main/java/com/jamie/springcloud/controller/RateLimitController.java
@@ -0,0 +1,49 @@
+package com.jamie.springcloud.controller;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.jamie.springcloud.entities.CommonResult;
+import com.jamie.springcloud.entities.Payment;
+import com.jamie.springcloud.myhandle.CustomerBlockHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RateLimitController {
+    /**
+     * 自定义限流时返回的错误页面
+     * @return
+     */
+    @GetMapping("/byResource")
+    @SentinelResource(value = "byResource", blockHandler = "handleException")
+    public CommonResult byResource() {
+        return new CommonResult(200, "按资源名称限流测试OK", new Payment(2020L, "serial001"));
+    }
+
+    public CommonResult handleException(BlockException exception) {
+        return new CommonResult(444, exception.getClass().getCanonicalName() + "\t 服务不可用");
+    }
+
+    /**
+     * 使用默认错误/限流页面
+     * @return
+     */
+    @GetMapping("/rateLimit/byUrl")
+    @SentinelResource(value = "byUrl")
+    public CommonResult byUrl() {
+        return new CommonResult(200, "按url限流测试OK", new Payment(2020L, "serial002"));
+    }
+
+
+    /**
+     * 全局自定义 处理
+     * @return
+     */
+    @GetMapping("/rateLimit/customerBlockHandler")
+    @SentinelResource(value = "customerBlockHandler",
+            blockHandlerClass = CustomerBlockHandler.class,
+            blockHandler = "handlerException2")
+    public CommonResult customerBlockHandler() {
+        return new CommonResult(200, "按客戶自定义", new Payment(2020L, "serial003"));
+    }
+}
