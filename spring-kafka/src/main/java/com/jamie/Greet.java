5aca033444ae7ced33097da66761b698d9a1b339
Jamie956 <752492509@qq.com>
2020-11-26 10:59:29 +0800

    up

--- spring-kafka/src/main/java/com/jamie/Greet.java
+++ spring-kafka/src/main/java/com/jamie/Greet.java
@@ -1,12 +1,23 @@
 package com.jamie;
 
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 public class Greet {
+    @Resource
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
     @RequestMapping("/")
     public String hi() {
         return "hihi";
+    }
+
+    @RequestMapping("/send")
+    public void send() {
+        kafkaTemplate.send("test_topic", "this is content");
     }
 }
