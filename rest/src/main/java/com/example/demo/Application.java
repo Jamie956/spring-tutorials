1d3b80aa7bb11f56612dd97b3563f3821964db85
Jamie956 <752492509@qq.com>
2020-12-10 15:06:02 +0800

    p

--- rest/src/main/java/com/example/demo/Application.java
+++ rest/src/main/java/com/example/demo/Application.java
@@ -2,11 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import java.io.File;
+import java.io.IOException;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(Application.class, args);
+
+		Resource resource = new ClassPathResource("/templates");
+		File file = resource.getFile();
+		System.out.println("==================="+file.toString());
 	}
 }
