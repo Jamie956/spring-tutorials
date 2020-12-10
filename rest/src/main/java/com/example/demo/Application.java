12a51af5dd025fad943cb1c1fe6136d21ff15686
Jamie956 <752492509@qq.com>
2020-12-10 18:38:52 +0800

    p

--- rest/src/main/java/com/example/demo/Application.java
+++ rest/src/main/java/com/example/demo/Application.java
@@ -7,6 +7,7 @@ import org.springframework.core.io.Resource;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 @SpringBootApplication
 public class Application {
@@ -14,8 +15,20 @@ public class Application {
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(Application.class, args);
 
-		Resource resource = new ClassPathResource("/templates");
-		File file = resource.getFile();
-		System.out.println("==================="+file.toString());
+		Resource resource = new ClassPathResource("/templates/aa.txt");
+		InputStream in = resource.getInputStream();
+		byte[] b = new byte[1024];
+		int len;
+		StringBuilder builder = new StringBuilder();
+		while ((len = in.read(b)) != -1) {
+			builder.append(new String(b, 0, len));
+		}
+		System.out.println("==================="+builder.toString());
+
+
+
+//		ClassPathResource resource = new ClassPathResource("/templates");
+//		File file = new File(resource.getPath());
+//		System.out.println("==================="+file.toString());
 	}
 }
