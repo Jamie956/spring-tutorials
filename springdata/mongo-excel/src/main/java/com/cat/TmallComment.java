57db02b4f57af58d8b75100a032303b22011706f
Jamie956 <752492509@qq.com>
2021-11-25 16:05:17 +0800

    update mongo-excel

--- springdata/mongo-excel/src/main/java/com/cat/TmallComment.java
+++ springdata/mongo-excel/src/main/java/com/cat/TmallComment.java
@@ -4,6 +4,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+/**
+ * mongo 天猫数据
+ */
 @Data
 @NoArgsConstructor
 @Document(collection = "tmallcomments")
@@ -11,13 +14,50 @@ public class TmallComment {
     private String _id;
     private String seller_id;
     private String goods_id;
+    /**
+     * 平台
+     */
+    private String platform = "天猫";
+    /**
+     * 产品分类
+     */
     private String goods_name;
-    private String rateDate;
-    private String price;
-    private String location;
-    private String shop_name;
+    /**
+     * 产品名称
+     */
     private String title;
-    private String detail_url;
-    private Integer commentSum;
+    /**
+     * 关键词命中
+     */
     private String keyword;
+    /**
+     * 品牌（无）
+     */
+    /**
+     * 全年评论数
+     */
+    private Integer commentSum;
+    /**
+     * 销量（无）
+     */
+    /**
+     * 最后一次更新时间
+     */
+    private String rateDate;
+    /**
+     * 价格
+     */
+    private String price;
+    /**
+     * 发货地
+     */
+    private String location;
+    /**
+     * 店铺名称
+     */
+    private String shop_name;
+    /**
+     * 网址链接
+     */
+    private String detail_url;
 }
