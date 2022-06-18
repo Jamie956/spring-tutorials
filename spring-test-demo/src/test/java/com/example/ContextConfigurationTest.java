ccbcf72c8b84e6eea3eeeb69e0de5eb3df8a8491
Jamie956 <752492509@qq.com>
2022-06-18 22:25:48 +0800

    mv to test

--- spring-test-demo/src/test/java/com/example/ContextConfigurationTest.java
+++ spring-test-demo/src/test/java/com/example/ContextConfigurationTest.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-
-
 /*
 @ContextConfiguration(classes = {SpringConfig.class})
 
