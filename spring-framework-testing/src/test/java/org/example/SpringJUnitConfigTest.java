cb3478f18aefa2ab7ebca23ca49b62294a0e50d0
Jamie956 <752492509@qq.com>
2022-12-17 21:36:26 +0800

    up

---
+++ spring-framework-testing/src/test/java/org/example/SpringJUnitConfigTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+@SpringJUnitConfig(X.class)
+public class SpringJUnitConfigTest {
+    @Autowired
+    private X x;
+
+    @Test
+    public void test1() {
+        Assert.assertEquals("foo", x.foo());
+    }
+
+}
