0f20abfdd1e6e3ab99c44bb4c91df27efbb58c3c
Jamie956 <752492509@qq.com>
2023-01-09 23:39:00 +0800

    spring mvc interceptor

--- spring-mvc-learn/demo4/src/main/java/org/example/HomeController.java
+++ spring-mvc-learn/demo4/src/main/java/org/example/HomeController.java
@@ -6,6 +6,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
+    /**
+     * test http:
+     *      method: get
+     *      url: http://localhost:8080/home/aa
+     * DispatcherServlet#doDispatch -> mappedHandler.applyPreHandle(..)
+     * DispatcherServlet#doDispatch -> mappedHandler.applyPostHandle(..)
+     * DispatcherServlet#doDispatch -> processDispatchResult(..)
+     */
     @RequestMapping("/aa")
     @ResponseBody
     public String index() {
