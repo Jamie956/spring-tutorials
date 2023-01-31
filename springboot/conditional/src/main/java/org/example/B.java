c08a482626c395910cf2e9552c51f76adb088d5d
Jamie956 <752492509@qq.com>
2023-01-31 09:36:43 +0800

    up

--- springboot/conditional/src/main/java/org/example/B.java
+++ springboot/conditional/src/main/java/org/example/B.java
@@ -1,13 +1,17 @@
 package org.example;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
 import org.springframework.stereotype.Component;
 
 @Component
-// create B when exist bean name a
+// create B if exist bean name a
 @ConditionalOnBean(name = "a")
-// create B when not exist bean name a1
+// create B if not exist bean name a1
 @ConditionalOnMissingBean(name = "a1")
+// create B if exist class A
+@ConditionalOnClass(value = {A.class})
 public class B {
 }
