3d59b2085b28f4d0dc1596273ada6c9dcacc3600
Jamie956 <752492509@qq.com>
2023-01-31 09:38:39 +0800

    up

--- springboot/conditional/src/main/java/org/example/B.java
+++ springboot/conditional/src/main/java/org/example/B.java
@@ -3,7 +3,6 @@ package org.example;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
 import org.springframework.stereotype.Component;
 
 @Component
