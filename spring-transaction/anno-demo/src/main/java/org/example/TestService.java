4266e80e0ca7e3658733ac78e81e0ab668339a57
Jamie956 <752492509@qq.com>
2023-02-05 16:46:05 +0800

    spring trx

---
+++ spring-transaction/anno-demo/src/main/java/org/example/TestService.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath*:spring.xml"})
+public class TestService {
+    @Autowired
+    private StaffService2 staffService;
+
+    @Test
+    public void testAdd() {
+        staffService.add();
+    }
+}
