efeeee390da2bf0329f1984367f70424019e878f
Jamie956 <752492509@qq.com>
2023-07-10 00:26:26 +0800

    remove

--- spring-framework-core/src/main/java/org/example/annotation_autowired/AnnotationAutowiredTest.java
+++ spring-framework-core/src/main/java/org/example/annotation_autowired/AnnotationAutowiredTest.java
@@ -1,6 +1,5 @@
 package org.example.annotation_autowired;
 
-import org.example.utils.DebugUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -11,19 +10,16 @@ public class AnnotationAutowiredTest {
         // Debug AnnotationConfigUtils.registerAnnotationConfigProcessors:
         //      register Spring internal bean include AutowiredAnnotationBeanPostProcessor
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-        DebugUtils.printBeanDefinition(context, "created context");
 
         // Debug ClassPathScanningCandidateComponentProvider.findCandidateComponents:
         //      path resolve finding candidate bean definition
         // Debug ClassPathBeanDefinitionScanner.registerBeanDefinition:
         //      register bean definition
         context.scan("org.example.annotation_autowired");
-        DebugUtils.printBeanDefinition(context, "finished scan");
 
         // Debug Z#setX: when populate bean
         // Instantiate Bean
         context.refresh();
-        DebugUtils.printBeanDefinition(context, "done");
 
         Assert.assertNotNull(context.getBean("x"));
         Assert.assertNotNull(context.getBean("y"));
